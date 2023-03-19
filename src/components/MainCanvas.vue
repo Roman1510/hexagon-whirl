d6cbc49709a9ee5e69f7245f421ca0d5357c6585
Roman1510 <romanvinnick@gmail.com>
2023-03-19 15:15:23 +0100

    base of the project

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -1,11 +1,24 @@
-<script setup></script>
+<script>
+export default {
+  mounted() {
+    const renderer = this.$refs.renderer
+    const box = this.$refs.box.mesh
+    renderer.onBeforeRender(() => {
+      box.rotation.x += 0.01
+    })
+  },
+}
+</script>
 
 <template>
-  <canvas></canvas>
+  <Renderer ref="renderer">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
+import { TroisJSVuePlugin } from 'troisjs'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(TroisJSVuePlugin).mount('#app')
